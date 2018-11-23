5fe1eacbb433bb165dff5e39463cec1e7de2bf08
Ding-Jie Lin <h94313@gmail.com>
2018-11-23 16:33:29 +0800

    jwt api route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,25 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('GetMonsters//{StartIndex}/{EndIndex}', function ($startId, $endId) {
+    return Redirect::to("GetMonsters/*/$startId/$endId");
+});
+Route::prefix('GetMonsters')->group(function () {
+    Route::get('{StartIndex}/{EndIndex}', function ($startId, $endId) {
+        return Redirect::to("GetMonsters/*/$startId/$endId");
+    });
+    Route::get('{fsString?}/{StartIndex?}/{EndIndex?}', 'api\MonsterController@show')
+        ->where(['StartIndex' => '[0-9]+', 'EndIndex' => '[0-9]+'])
+        ->name('GetMonsters');
+});
+Route::prefix('Image')->group(function () {
+    Route::get('{size}/{monId}/', function ($size, $monId) {
+        return Redirect::to("Image/$size/$size/$monId/0");
+    });
+    Route::get('{size}/{monId}/{imgId}', function ($size, $monId, $imgId) {
+        return Redirect::to("Image/$size/$size/$monId/$imgId");
+    });
+    Route::get('{width}/{height}/{monId}/{imgId}', 'api\ImageController@show');
 });
